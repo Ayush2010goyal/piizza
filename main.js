569365238214a64458910318f6c4d010b4fa6bf4
Ayush2010goyal <75526409+Ayush2010goyal@users.noreply.github.com>
2021-04-17 17:45:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+menu_list_array=["Chicken Tandoori Pizza","Veg Supreme Pizza", "Paneer Tikka Pizza","Deluxe Veggie Pizza ","Veg Extravaganza Pizza"];
+
+
+function getMenu(){
+    var htmlData;
+    htmlData="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++)
+    {
+        htmlData=htmlData+'<li>'+menu_list_array[i]+'</li>'
+    }
+    htmlData=htmlData+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmlData;
+}
+
+function add_item(){
+    var htmlData;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++)
+    {
+        htmlData=htmlData+'<div class="card">'+'<img src="pizzaImg.png"/>'+menu_list_array[i]+'</div>'
+    }
+    htmlData=htmlData+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmlData;
+}
